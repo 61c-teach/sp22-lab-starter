c8fa5069f7f4ec3b8a8241f1c07efe65b817ad5a
Oski Bear <oski@example.com>
2021-08-30 15:39:46 +0000

    [lab00] fix fizzbuzz

--- lab00/code.py
+++ lab00/code.py
@@ -5,9 +5,11 @@ def get_airspeed_velocity_of(unladen_swallow):
     return # redacted
 
 def fizzbuzz(num):
-  if num == 3: # edit this line
+  if str(num) in ["15"]:
+    print(f"{num}: fizzbuzz")
+  elif str(num) in ["3", "6", "9", "12", "15", "18"]:
     print(f"{num}: fizz")
-  if num == 5: # edit this line
+  elif str(num) in ["5", "10", "15"]:
     print(f"{num}: buzz")
 
 for i in range(1, 20):
